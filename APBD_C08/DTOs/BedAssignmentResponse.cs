2d341561f70b06f0b8fdc19df802c1d18c11532e
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-05-20 13:20:50 +0200

    Implemented missing DTOs for second endpoint

--- APBD_C08/DTOs/BedAssignmentResponse.cs
+++ APBD_C08/DTOs/BedAssignmentResponse.cs
@@ -2,5 +2,12 @@
 
 public class BedAssignmentResponse
 {
+    public int Id { get; set; }
+    public string PatientPesel { get; set; } = string.Empty;
     
+    public int BedId { get; set; }
+    public DateTime From { get; set; }
+    public DateTime? To { get; set; }
+    
+    public BedDto Bed { get; set; } = null!;
 }
--- APBD_C08/DTOs/CreateBedAssignmentRequest.cs
+++ APBD_C08/DTOs/CreateBedAssignmentRequest.cs
@@ -1,6 +1,18 @@
 ﻿namespace APBD_C08.DTOs;
+using System;
+using System.ComponentModel.DataAnnotations;
+
 
 public class CreateBedAssignmentRequest
 {
-    
+    [Required(ErrorMessage = "WardId is required.")]
+    public int WardId { get; set; }
+
+    [Required(ErrorMessage = "BedTypeId is required.")]
+    public int BedTypeId { get; set; }
+
+    [Required(ErrorMessage = "Start date (From) is required.")]
+    public DateTime From { get; set; }
+
+    public DateTime? To { get; set; }
 }
